f76db10d1e934a076da3f56753a3736f287f8f58
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-08-28 13:27:24 +0900

    プログレスバー

--- fyne-sample.go
+++ fyne-sample.go
@@ -6,17 +6,21 @@ import (
 )
 
 func main() {
+	v := 0.0
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	sl := widget.NewSelect([]string{
-		"Eins", "Twei", "Drei",
-	}, func(s string) {
-		l.SetText("selected: " + s)
+	p := widget.NewProgressBar()
+	b := widget.NewButton("Up!", func() {
+		v += 0.1
+		if v > 1.0 {
+			v = 0
+		}
+		p.SetValue(v)
 	})
 	w.SetContent(
 		widget.NewVBox(
-			l, sl,
+			l, p, b,
 		),
 	)
 	w.ShowAndRun()
