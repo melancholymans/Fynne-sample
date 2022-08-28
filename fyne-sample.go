901415bfae1639cfeb53b2ff452f606395661e9f
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-08-28 13:08:09 +0900

    選択リスト

--- fyne-sample.go
+++ fyne-sample.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
@@ -11,13 +9,14 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	s := widget.NewSlider(0.0, 100.0)
-	b := widget.NewButton("Check", func() {
-		l.SetText("value: " + strconv.Itoa(int(s.Value)))
+	sl := widget.NewSelect([]string{
+		"Eins", "Twei", "Drei",
+	}, func(s string) {
+		l.SetText("selected: " + s)
 	})
 	w.SetContent(
 		widget.NewVBox(
-			l, s, b,
+			l, sl,
 		),
 	)
 	w.ShowAndRun()
